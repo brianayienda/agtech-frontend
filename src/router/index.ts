f3f9361924a6d565b2573e0f1683a3af064762b9
brianayienda <ayiendabrian37@gmail.com>
2025-09-01 14:55:15 +0300

    Login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Dashboard from '../views/Dashboard.vue';
+import Login from '../views/Login.vue';
 
 const routes = [
   { path: '/', name: 'Dashboard', component: Dashboard },
+  { path: '/login', name: 'Login', component: Login },
 ];
 
 const router = createRouter({
