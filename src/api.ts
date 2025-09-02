bbbc245e903325f3750e8763fef47a9f007ab6cf
brianayienda <ayiendabrian37@gmail.com>
2025-09-02 06:27:27 +0300

    Remove default localhost

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,14 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
 });
 
-// ✅ Fix type: use InternalAxiosRequestConfig
+// Fix type: use InternalAxiosRequestConfig
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   const token = localStorage.getItem("token");
   if (token) {
