d0650081c07ce4174f82c2a268536f714f8da6ed
brianayienda <ayiendabrian37@gmail.com>
2025-09-02 08:44:49 +0300

    Links uodate

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -6,7 +6,7 @@
       <nav class="p-4 space-y-2">
         <RouterLink to="/admin" class="block p-2 rounded hover:bg-gray-200">Dashboard</RouterLink>
         <RouterLink to="/admin/farmers" class="block p-2 rounded hover:bg-gray-200">Farmers</RouterLink>
-        <RouterLink to="/crops" class="block p-2 rounded hover:bg-gray-200">Crops</RouterLink>
+        <RouterLink to="/admin/crops" class="block p-2 rounded hover:bg-gray-200">Crops</RouterLink>
         <RouterLink to="/admin/profile" class="block p-2 rounded hover:bg-gray-200">Profile</RouterLink>
         <button @click="logout" class="w-full text-left p-2 rounded bg-red-500 text-white hover:bg-red-600">
           Logout
--- src/layouts/FarmerLayout.vue
+++ src/layouts/FarmerLayout.vue
@@ -5,7 +5,7 @@
       <div class="p-6 font-bold text-xl border-b">Farmer Panel</div>
       <nav class="p-4 space-y-2">
         <RouterLink to="/farmer" class="block p-2 rounded hover:bg-gray-200">Dashboard</RouterLink>
-        <RouterLink to="/crops" class="block p-2 rounded hover:bg-gray-200">Crops</RouterLink>
+        <RouterLink to="/farmer/crops" class="block p-2 rounded hover:bg-gray-200">Crops</RouterLink>
         <RouterLink to="/farmer/profile" class="block p-2 rounded hover:bg-gray-200">Profile</RouterLink>
         <button @click="logout" class="w-full text-left p-2 rounded bg-red-500 text-white hover:bg-red-600">
           Logout
